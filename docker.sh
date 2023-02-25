6cdd8915de34b4d1cf5ad76ea60546104f5e75af
tfuji <tomofuji.dev@gmail.com>
2023-02-25 13:14:49 +0900

    add docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,2 @@
-docker run -it -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu /bin/bash
+# docker container run -it -d --name myubuntu --platform=linux/amd64 -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu:latest
+docker container exec -it myubuntu /bin/bash
