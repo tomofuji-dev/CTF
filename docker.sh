301214cd098ae5abbe7d5812471d6ac61ba61bb6
tfuji <tomofuji.dev@gmail.com>
2023-03-01 17:27:15 +0900

    0301

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 # docker container run -it -d --name myubuntu --platform=linux/amd64 -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu:latest
-docker container exec -it myubuntu /bin/bash
+docker container exec -it ctfubuntu /bin/bash
