ba0b3a89a00573902b8bf189998c5dae85103379
tfuji <tomofuji.dev@gmail.com>
2023-03-03 13:44:08 +0900

    solve ~Q21

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
-# docker container run -it -d --name myubuntu --platform=linux/amd64 -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu:latest
+docker container run -it -d --name ctfub --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" --platform=linux/amd64 -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu:latest
 docker container exec -it ctfubuntu /bin/bash
