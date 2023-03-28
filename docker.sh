9506e8d68117e1cb4f9e61699e7a17360148f2d3
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2023-03-28 11:53:40 +0900

    modify docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
-docker container run -it -d --name ctfub --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" --platform=linux/amd64 -v /Users/t.fuji/Dev/42/cursus/.playground/CpawCTF:/CpawCTF ubuntu:latest
+docker container run -it -d --name ctfubuntu --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v /Users/t.fuji/Dev/42/learning/CTF:/CTF ubuntu:latest
 docker container exec -it ctfubuntu /bin/bash
